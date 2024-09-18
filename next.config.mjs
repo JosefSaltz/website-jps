6e7eda8d4c42b7864497e5cde69882ba955f564b
Josef Saltz <JosefSaltz@users.noreply.github.com>
2024-09-18 13:34:24 -0700

    (setup): Boilerplate config files
    This included merging the template and ensuring all the typescript and other project tooling features I wanted to use are compatible with each other and running other the same expectations. Such as tsconfig src/ aliases and type deps for other packages.

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+import rehypePrism from '@mapbox/rehype-prism';
+import nextMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
+
+export const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/articles/*': ['./src/app/articles/**/*.mdx'],
+    },
+  }
+};
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig);
