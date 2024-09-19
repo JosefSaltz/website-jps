8cb792ff65806a7a5f2cbbc366ece183c093825b
Josef Saltz <JosefSaltz@users.noreply.github.com>
2024-09-19 11:54:30 -0700

    fix(compile): Fix compiler target and even out diff of proj and template

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
 import rehypePrism from '@mapbox/rehype-prism';
 import nextMDX from '@next/mdx';
 import remarkGfm from 'remark-gfm';
+/** @type {import('next').NextConfig} */
 
-export const nextConfig = {
+const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   experimental: {
     outputFileTracingIncludes: {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,20 +4,10 @@ import typographyPlugin from '@tailwindcss/typography';
 
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'selector',
   plugins: [typographyPlugin],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
     fontSize: {
       xs: ['0.8125rem', { lineHeight: '1.5rem' }],
       sm: ['0.875rem', { lineHeight: '1.5rem' }],
