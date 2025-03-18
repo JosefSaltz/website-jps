5f389847fd36fb46d17252299dd510fe3b141b07
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-17 20:57:12 -0700

    init(migrate): Move to a svelte kit project and boilerplate

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import aspectRatio from '@tailwindcss/aspect-ratio';
+import containerQueries from '@tailwindcss/container-queries';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+    extend: {
+      animation: {
+        meteor: "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: '1' },
+          "70%": { opacity: '1' },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: '0',
+          },
+        },
+      },
+    },
+  },
+
+	plugins: [typography, forms, containerQueries, aspectRatio]
+} satisfies Config;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		enhancedImages(), 
+		sveltekit()
+	],
+	server: {
+		port: 3000
+	}
+});
