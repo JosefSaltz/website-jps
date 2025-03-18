4313b9955e72de2a0b21e95b6f3f07eadf2ae884
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-17 20:57:58 -0700

    add(ui): Add basic navbar

---
+++ src/lib/components/Navbar/Navbar.svelte
@@ -0,0 +1,22 @@
+<nav class="flex flex-row justify-end h-16 border-b-2 border-black">
+  <ul class="list-none flex flex-row space-x-6 pr-8 items-center lg:text-lg font-semibold">
+    <li><a href="#about">About</a></li>
+    <li><a href="#portfolio">Portfolio</a></li>
+    <li><button>Contact</button></li>
+  </ul>
+</nav>
+
+<style>
+  li {
+    padding: 1rem;
+    margin: 1rem;
+  }
+  
+  li:hover {
+    background-color: rgb(15 23 42 / var(--tw-bg-opacity, 1));
+    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.5);
+    border-radius: 5px;
+    color: white;
+  }
+</style>
+
