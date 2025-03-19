5bb132e382f88bd4ca817e49267c1598031e45df
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-18 21:57:03 -0700

    add(ContactForm, GridPattern): Add new form and Magic UI Component

---
+++ src/lib/components/Forms/ContactForm.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { enhance } from "$app/forms";
+</script>
+
+<div class="rounded-md border-2 p-16 space-y-6">
+  <h3 class="text-3xl">Contact Me</h3>
+  <form use:enhance>
+    <ul class="space-y-6">
+      <li>
+        <label for="name">Name</label>
+        <input name="name" type="text">
+      </li>
+      <li>
+        <label for="email">Email</label>
+        <input name="email" type="text" required>
+      </li>
+      <li>
+        <label for="message">Message</label>
+        <input name="message" type="text" required>
+      </li>
+      <div class="w-full flex justify-center">
+        <button class="w-20 self-center bg-cyan-800 border-solid border-zinc-700 rounded-lg py-3" type="submit">Send</button>
+      </div>
+  </form>
+</div>
+
+<style>
+  input {
+    list-style: none;
+  }
+</style>
