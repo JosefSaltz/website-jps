19145b93d847c72bcbebe94c70597464ed66460e
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-18 20:41:26 -0700

    refactor(MagicUI): Place MagicUI components under a common folder

--- src/lib/components/MagicUI/Meteors.svelte
+++ src/lib/components/MagicUI/Meteors.svelte
@@ -2,18 +2,23 @@
   import { cn } from "$lib/utils";
   import { onMount } from "svelte";
 
-  let { number } = $props();
+  let { number = 10 } = $props();
 
   let meteorStyles: any = $state([]);
   let changeMeteors = (num: number) => {
     meteorStyles = [];
     const styles = [...new Array(num)].map(() => {
+      const leftVal = Math.floor(Math.random() * 700);
+      // Spawn potentially between -40vw and up to 100+ vw for even spread
+      const left = (Math.random() * -40) + (Math.random() * 120) + "vw"
+      
       return {
       top: -20,
-      left: Math.floor(Math.random() * 700 - 10) + "vw",
-      animationDelay: Math.random() * 7 + 0.2 + "s",
+      left,
+      animationDelay: Math.random() * 1 + 0.2 + "s",
       animationDuration: Math.floor(Math.random() * 8 + 2.9) + "s",
-    }});
+      }
+    });
     meteorStyles = styles;
   };
   onMount(() => {
@@ -26,9 +31,14 @@
   <span
     id="meteor-{idx+1}"
     class={cn(
-      "pointer-events-none absolute left-1/2 top-1/2 size-[2.4px] rotate-[215deg] animate-meteor rounded-full bg-slate-500 shadow-[0_0_0_1px_#ffffff10]"
+      "pointer-events-none absolute size-[2.4px] rotate-[215deg] animate-meteor rounded-full bg-slate-500 shadow-[0_0_0_1px_#ffffff10]"
     )}
-    style="top: {style.top}px; left: {style.left}; animation-delay: {style.animationDelay}; animation-duration: {style.animationDuration};"
+    style="
+      top: {style.top}px; 
+      left: {style.left}; 
+      animation-delay: {style.animationDelay}; 
+      animation-duration: {style.animationDuration};
+    "
   >
     <!-- Meteor Tail  -->
     <div
