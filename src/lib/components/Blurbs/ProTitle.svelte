5be3ebd0908e874c8ef79449684cacafef563c02
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-18 20:42:21 -0700

    refactor(ProTitle): Extrapolate the pro title blurb to it's own component

---
+++ src/lib/components/Blurbs/ProTitle.svelte
@@ -0,0 +1,7 @@
+<!-- Name/Title -->
+<div id="pro-title" class="w-fit space-y-2 ">
+  <div class="border-b-sky-700 border-b-4 text-slate-900">
+    <h2 class="text-6xl font-semibold place-self-end">Josef Saltz</h2>
+    <h2 class="text-5xl self-end pb-2">Full Stack Developer</h2>
+  </div>
+</div>
