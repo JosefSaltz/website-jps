ab3245a2a6c787ddb87cc513c8476c6fca9a30f1
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-19 00:27:16 -0700

    add(Portfolio): Add Portfolio components and children

---
+++ src/lib/components/Portfolio/WorkCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  type Props = {
+    name: string,
+    link?: string
+    thumbnail: string
+  }
+  let { name, link, thumbnail }: Props = $props()
+</script>
+
+<div class="rounded-lg shadow-md aspect-[3/2] min-w-10 max-w-40 bg-[{thumbnail}] text-center">
+  <a href={link}>{name}</a>
+</div>
