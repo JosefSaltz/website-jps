5bbde51abc4ec528d8875239c8a18fa25360df3e
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-20 14:35:28 -0700

    update(meteorcard): Add gradient and make some responsive layout changes
    Damn that gradient looks good

--- src/lib/components/Hero/Hero.svelte
+++ src/lib/components/Hero/Hero.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import GridPattern from "$components/MagicUI/Backgrounds/GridPattern.svelte";
   import Meteors from "$components/MagicUI/Meteors.svelte";
-  import { cn } from "$lib/utils";
 	import TitleText from "./TitleText.svelte";
   import Navbar from "$components/Navbar/Navbar.svelte";
+  import { cn } from "$lib/utils";
+	import LetsGoButton from "./LetsGoButton.svelte";
 </script>
 
-<div id="hero" class="flex flex-1 flex-col justify-center items-center w-full h-screen bg-zinc-900">
+<div id="hero" class="flex flex-1 flex-col sm:justify-center items-center w-full h-screen bg-zinc-900">
   <Navbar />
   <GridPattern
     width={30}
@@ -17,17 +18,15 @@
     )}
   />
   
-  <div id="meteor-title-card" class="relative rounded-xl bg-slate-900 shadow-black shadow-xl border-slate-800 border-[1px] overflow-hidden">
+  <div id="meteor-title-card" class="relative flex justify-center items-center h-screen sm:h-auto sm:rounded-xl bg-gradient-to-br from-slate-900 to-black shadow-black shadow-xl border-slate-800 border-[1px] overflow-hidden">
     <Meteors number={200} />
     <!-- Splash Text -->
     <div 
       id="container-title-text" 
-      class="w-full max-w-3xl p-40 bg-clip-text text-white">
-      <TitleText />
+      class="w-full max-w-3xl p-20 lg:p-40 bg-clip-text text-white">
+      <TitleText className={`absolute`} />
     </div>
+    <LetsGoButton className={"sm:hidden"} />
   </div>
-
-  <div class="flex-col flex-shrink justify-center">
-    <a href="#about" class="text-white text-center">Let's Go</a>
-  </div> 
+  <LetsGoButton className={"hidden sm:block"} />
 </div>
---
+++ src/lib/components/Hero/LetsGoButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  type Props = { className?: string }
+  let { className }: Props = $props()
+</script>
+
+<div class={`${className} flex-col flex-shrink justify-center`}>
+  <a href="#about" class="text-white text-center">Let's Go</a>
+</div> 
