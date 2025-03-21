d1087c6e417460b53b48eba3f7db8b599d43d1cd
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-20 17:21:41 -0700

    remove(portfolio): remove redundant component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,10 @@
 	import About from "$components/About/About.svelte";
 	import ContactForm from "$components/Forms/ContactForm.svelte";
 	import Hero from "$components/Hero/Hero.svelte";
-	import Portfolio from "$components/Portfolio/Portfolio.svelte";
 	import Skills from "$components/Skills/Skills.svelte";
 </script>
 
 <Hero />
 <About />
 <Skills />
-<Portfolio />
 <ContactForm />
