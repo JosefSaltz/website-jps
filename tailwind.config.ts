9d0477de5132f958ec49ddccadd0f78f1f5cc21d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-03-20 17:20:08 -0700

    add(magicui): Add shimmerbutton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
     extend: {
       animation: {
         meteor: "meteor 5s linear infinite",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+        magicslide: "magicslide var(--speed) ease-in-out infinite alternate",
       },
       keyframes: {
         meteor: {
@@ -19,6 +21,25 @@ export default {
           "100%": {
             transform: "rotate(215deg) translateX(-500px)",
             opacity: '0',
+          },
+        },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        magicslide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
           },
         },
       },
