8759558b4e207440ca58fa34a8d55afff1caf9c4
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-04-01 01:12:44 -0700

    update(ContactForm): Tweak layout

--- src/lib/components/Forms/ContactForm.svelte
+++ src/lib/components/Forms/ContactForm.svelte
@@ -2,9 +2,9 @@
 	import { enhance } from "$app/forms";
 </script>
 
-<div id="contact" class="h-screen rounded-md border-2 p-16 space-y-6">
-  <h3 class="text-3xl">Work With Me</h3>
-  <form use:enhance>
+<div id="contact" class="h-screen w-full pt-10 flex flex-col items-center">
+  <h3 class="text-5xl">Work With Me</h3>
+  <form use:enhance method="POST">
     <ul class="space-y-6">
       <li>
         <label for="name">Name</label>
@@ -14,9 +14,11 @@
         <label for="email">Email</label>
         <input name="email" type="text" required>
       </li>
-      <li>
+      <li class="flex-2">
         <label for="message">Message</label>
-        <input name="message" type="text" required>
+        <input name="message" type="text" required class="
+          
+        ">
       </li>
       <div class="w-full flex justify-center">
         <button class="w-20 self-center bg-cyan-800 border-solid border-zinc-700 rounded-lg py-3" type="submit">Send</button>
@@ -27,5 +29,10 @@
 <style>
   input {
     list-style: none;
+  }
+
+  li {
+    display: flex;
+    flex-direction: column;
   }
 </style>
