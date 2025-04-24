e8cfe0aaa1974e023d65f01282a9638ee10b34a0
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-04-23 19:21:58 -0700

    update(ContactForm): Add blurb content

--- src/lib/components/Forms/ContactForm.svelte
+++ src/lib/components/Forms/ContactForm.svelte
@@ -4,6 +4,11 @@
 
 <div id="contact" class="h-screen w-full pt-10 flex flex-col items-center">
   <h3 class="text-5xl">Work With Me</h3>
+  <h5>Let’s Build Something Great Together!</h5>
+
+  <p>Ready to bring your web project to life? Whether you need a stunning website, a powerful web app, or expert advice on your next digital venture, I’m here to help. Reach out today and let’s turn your ideas into reality!</p>
+    
+  <p>Contact me now to get started.</p>
   <form use:enhance method="POST">
     <ul class="space-y-6">
       <li>
