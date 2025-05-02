efa37f0fe4628d55c7fde3a22d215f36c9fbb60e
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-01 19:56:42 -0700

    update(About): Update layouts with Ashley's professional opinion

--- src/lib/components/About/About.svelte
+++ src/lib/components/About/About.svelte
@@ -7,15 +7,19 @@
   import CombinedAppGear from "$lib/assets/svg/combined-app-gear.svelte";
 	import ServiceTile from "./ServiceTile.svelte";
 	import WorkCarousel from "$components/WorkCarousel/WorkCarousel.svelte";
+  import selfie from "$assets/img/selfie.webp"
 </script>
 
 <div id="about" class="min-h-screen flex flex-col items-center bg-white py-20 space-y-14">
-  <div class="w-full xl:max-w-6xl px-10 md:px-20 flex flex-row justify-end justify-self-center ">
-    <ProTitle />
-  </div>
-  <div id="summary-container" class="max-w-6xl px-10 md:px-20">
-    <h4 class="text-3xl md:text-4xl font-semibold mb-2 md:mb-4">About</h4>
-    <Summary />
+  <div id="personal-section" class="flex flex-row-reverse p-10">
+    <img class="w-auto rounded-sm h-[24em] md:h-[48em]" src={selfie} alt="self-portrait">
+    <div class="flex flex-col justify-center pb-24 space-y-16">
+      <ProTitle class="px-20" />
+      <div id="summary-container" class="max-w-6xl px-10 md:px-20">
+        <h4 class="text-3xl md:text-4xl font-semibold mb-2 md:mb-4">About</h4>
+        <Summary />
+      </div>
+    </div>
   </div>
   <div id="services" class="w-full md:max-w-6xl flex flex-col">
     <h1 class="text-4xl font-semibold mb-8 self-start px-10 md:px-20">Services</h1>
--- src/lib/components/About/ProTitle.svelte
+++ src/lib/components/About/ProTitle.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+  let { class: className = '' } = $props()
+</script>
+
 <!-- Name/Title -->
-<div id="pro-title" class="w-fit">
+<div id="pro-title" class={`w-fit ${className}`}>
   <div class="border-b-sky-700 border-b-4 text-slate-900">
-    <h2 class="text-4xl md:text-6xl font-semibold place-self-end">Josef Saltz</h2>
-    <h2 class="text-2xl md:text-5xl self-end pb-2">Full Stack Developer</h2>
+    <h2 class="text-2xl md:text-6xl font-semibold place-self-end">Josef Saltz</h2>
+    <h2 class="text-xl md:text-5xl self-end pb-2">Full Stack Developer</h2>
   </div>
 </div>
