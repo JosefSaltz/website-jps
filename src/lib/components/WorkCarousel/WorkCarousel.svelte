31d4069cd2d47d5ddb8a3528d7221226ab95e870
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-01 21:35:46 -0700

    add(WorkCarousel): Adds a carousel for past work thumbnails

--- src/lib/components/WorkCarousel/WorkCarousel.svelte
+++ src/lib/components/WorkCarousel/WorkCarousel.svelte
@@ -5,27 +5,44 @@
   import Piita from "$lib/assets/img/piita.png"
   import WorkCard from "$components/Portfolio/WorkCard.svelte";
   import emblaCarouselSvelte from 'embla-carousel-svelte'
+  import Autoplay, { type AutoplayType } from 'embla-carousel-autoplay'
+
+  let options = { loop: false }
+  let plugins = [Autoplay()] satisfies AutoplayType[];
+
+  const workCards = [ 
+  {
+    name: "PIITA", 
+    link: "https://piita.org", 
+    thumbnail: Piita
+  },
+  {
+    name: "Company C Ballet", 
+    link: "https://web.archive.org/web/20171102033455/http://www.companycballet.org/", 
+    thumbnail: CompanyC
+  },
+  {
+    name: "OneSimplePhone", 
+    link: "https://smscola.vippbx.com", 
+    thumbnail: OneSP
+  },
+  {
+    name: "KeepOurNetFree", 
+    link: "https://web.archive.org/web/20170329174527/http://keepournetfree.org/", 
+    thumbnail: KeepOurNetFree
+  },
+];
 </script>
 
 <div id="work" class="flex flex-col w-full max-w-6xl px-20">
   <h3 class="text-4xl font-semibold mb-4">My Work</h3>
-  <div class="embla" use:emblaCarouselSvelte>
+  <div class="embla" use:emblaCarouselSvelte={{options, plugins}}>
     <div class="embla__container">
-      <div class="embla__slide">
-        <WorkCard name={"PIITA"} link={"https://piita.org"} thumbnail={Piita} />
-      </div>
-
-      <div class="embla__slide">
-        <WorkCard name={"Company C Ballet"} thumbnail={CompanyC} />
-      </div>
-
-      <div class="embla__slide">
-        <WorkCard name={"OneSimplePhone"} link={"https://smscola.vippbx.com"} thumbnail={OneSP} />
-      </div>
-
-      <div class="embla__slide">
-        <WorkCard name={"KeepOurNetFree"} thumbnail={KeepOurNetFree} />
-      </div>
+      {#each workCards as { name, link, thumbnail }}
+        <div class="embla__slide">
+          <WorkCard {name} {link} {thumbnail} />
+        </div>
+      {/each}
     </div>
   </div>
 </div>
