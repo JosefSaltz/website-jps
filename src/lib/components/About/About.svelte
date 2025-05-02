101f0a4813e7ae0ad83ad7c50aabc557d11235c2
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-01 21:35:24 -0700

    refactor(OfferedServices): Extrapolate services section to it's own component

--- src/lib/components/About/About.svelte
+++ src/lib/components/About/About.svelte
@@ -1,34 +1,23 @@
 <script lang="ts">
 	import ProTitle from "./ProTitle.svelte";
 	import Summary from "./Summary.svelte";
-  import chatIcon from "$lib/assets/svg/chat.svelte"
-  import computerIcon from "$lib/assets/svg/computer.svelte"
-  import serverIcon from "$lib/assets/svg/server.svelte"
-  import CombinedAppGear from "$lib/assets/svg/combined-app-gear.svelte";
-	import ServiceTile from "./ServiceTile.svelte";
+
 	import WorkCarousel from "$components/WorkCarousel/WorkCarousel.svelte";
   import selfie from "$assets/img/selfie.webp"
+	import OfferedServices from "$components/OfferedServices/OfferedServices.svelte";
 </script>
 
 <div id="about" class="min-h-screen flex flex-col items-center bg-white py-20 space-y-14">
   <div id="personal-section" class="flex flex-row-reverse p-10">
-    <img class="w-auto rounded-sm h-[24em] md:h-[48em]" src={selfie} alt="self-portrait">
-    <div class="flex flex-col justify-center pb-24 space-y-16">
-      <ProTitle class="px-20" />
-      <div id="summary-container" class="max-w-6xl px-10 md:px-20">
-        <h4 class="text-3xl md:text-4xl font-semibold mb-2 md:mb-4">About</h4>
-        <Summary />
+    <img class="w-auto rounded-sm ml-6 mb-6 xl:m-0 h-[16em] sm:h-[24em] md:h-[48em]" src={selfie} alt="self-portrait">
+    <div class="md:flex md:flex-col md:justify-center md:pb-24 md:space-y-16">
+      <ProTitle class="mb-10" />
+      <div id="summary-container" class="w-full max-w-6xl md:px-20">
+        <h4 class="text-2xl md:text-4xl font-semibold mb-2 md:mb-4">About</h4>
+        <Summary class="md:pr-16" />
       </div>
     </div>
   </div>
-  <div id="services" class="w-full md:max-w-6xl flex flex-col">
-    <h1 class="text-4xl font-semibold mb-8 self-start px-10 md:px-20">Services</h1>
-    <div id="services-tile-container" class="flex flex-wrap md:flex-row justify-center sm:space-x-8 md:space-x-18">
-      <ServiceTile name={'App Development'} icons={[CombinedAppGear]} />
-      <ServiceTile name={'Consulting'} icons={[chatIcon]} />
-      <ServiceTile name={'Hosting'} icons={[serverIcon]} />
-      <ServiceTile name={'Web Sites'} icons={[computerIcon]} />
-    </div>
-  </div>
+  <OfferedServices />
   <WorkCarousel />
 </div>
---
+++ src/lib/components/OfferedServices/OfferedServices.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import chatIcon from "$lib/assets/svg/chat.svelte"
+  import computerIcon from "$lib/assets/svg/computer.svelte"
+  import serverIcon from "$lib/assets/svg/server.svelte"
+  import CombinedAppGear from "$lib/assets/svg/combined-app-gear.svelte";
+	import ServiceTile from "$components/OfferedServices/ServiceTile.svelte";
+</script>
+
+<div id="services" class="flex flex-col">
+  <h1 class="text-4xl font-semibold mb-8 self-start">Services</h1>
+  <div id="services-tile-container" class="flex flex-wrap md:flex-row justify-center sm:space-x-8 md:space-x-18">
+    <ServiceTile name={'App Development'} icons={[CombinedAppGear]} />
+    <ServiceTile name={'Consulting'} icons={[chatIcon]} />
+    <ServiceTile name={'Hosting'} icons={[serverIcon]} />
+    <ServiceTile name={'Web Sites'} icons={[computerIcon]} />
+  </div>
+</div>
