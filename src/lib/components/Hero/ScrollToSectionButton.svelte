bc93873bd0e31a867e2ba9c81a66a0c86d404ba0
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-04 13:52:51 -0700

    new(ScrollToSectionButton): Modular variant of the LetsGoButton

---
+++ src/lib/components/Hero/ScrollToSectionButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ShimmerButton from "$components/MagicUI/ShimmerButton.svelte";
+  
+  let { class: className ='', targetID ="#", buttonText = '' } = $props()
+</script>
+
+<div class={`${className}`}>
+  <a href={targetID} class="text-white text-center">
+    <ShimmerButton>
+      { buttonText }
+    </ShimmerButton>
+  </a>
+</div> 
