8c33976076b59a0eab1a20188688b6ce10b2f35d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-04 18:20:20 -0700

    update(ProTitle): Add more sizing options

--- src/lib/components/About/ProTitle.svelte
+++ src/lib/components/About/ProTitle.svelte
@@ -4,6 +4,6 @@
 
 <!-- Name/Title -->
 <div id="pro-title" class={`w-fit  text-slate-900 ${className}`}>
-    <h2 class="text-3xl md:text-6xl font-semibold place-self-end">Josef Saltz</h2>
-    <h2 class="text-2xl md:text-5xl pb-2 self-end border-b-sky-700 border-b-4">Full Stack Developer</h2>
+    <h2 class="text-3xl md:text-5xl xl:text-6xl font-semibold place-self-end">Josef Saltz</h2>
+    <h2 class="text-2xl md:text-4xl xl:text-5xl pb-2 self-end border-b-sky-700 border-b-4">Full Stack Developer</h2>
 </div>
