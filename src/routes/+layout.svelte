7cff2a52921af49775ed08c31f5e8e4a8f9a9f0e
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-04 20:23:36 -0700

    fix(prerender): Move to correct route file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	import Navbar from '$components/Navbar/Navbar.svelte';
 	import '../app.css';
 	let { children } = $props();
-	// This can be false if you're using a fallback (i.e. SPA mode)
-	export const prerender = true;
 </script>
 
 {@render children()}
