fa42c7ac2f678bd9c9cc4c93d5def15a765aede5
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-04 20:12:43 -0700

    fix(layout): Add missing root prerender option

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Navbar from '$components/Navbar/Navbar.svelte';
-import '../app.css';
+	import '../app.css';
 	let { children } = $props();
+	// This can be false if you're using a fallback (i.e. SPA mode)
+	export const prerender = true;
 </script>
 
 {@render children()}
