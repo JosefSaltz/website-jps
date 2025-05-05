fa91e99caf036de08b78015b36ee4c4a9e3d5545
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-04 23:08:14 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import deno from "@deno/vite-plugin"
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
+		deno(),
 		sveltekit(),
 		enhancedImages(), 
 	],
