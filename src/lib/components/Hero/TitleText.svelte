02b4cd6ae951f6b89224f0e28df735464591376d
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-05 00:31:51 -0700

    update(TitleText): Add more responsive sizes

--- src/lib/components/Hero/TitleText.svelte
+++ src/lib/components/Hero/TitleText.svelte
@@ -4,10 +4,10 @@
 
 <h1 
   class={`
-    text-4xl md:text-5xl xl:text-7xl  
+    text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl 
     font-semibold tracking-tight whitespace-pre-wrap
     bg-gradient-to-br from-white to-slate-500/40 
     text-transparent bg-clip-text ${className}
   `}>
-  Build With The Power Of The <span class="text-sky-800">Web</span>
+  Build With The Power Of The <span class="text-sky-700">Web</span>
 </h1>
