764490b6f2300f937358971f1ae094dbd0c3b1d2
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-05-12 23:26:30 -0700

    update(TitleText): Adds a fixed character width, bg-gradient, move z position

--- src/lib/components/Hero/TitleText.svelte
+++ src/lib/components/Hero/TitleText.svelte
@@ -4,10 +4,11 @@
 
 <h1 
   class={`
-    text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl 
+    w-[10ch] text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl
+    relative z-50 
     font-semibold tracking-tight whitespace-pre-wrap
     bg-gradient-to-br from-white to-slate-500/40 
     text-transparent bg-clip-text ${className}
   `}>
-  Build With The Power Of The <span class="text-sky-700">Web</span>
+  Build With The Power Of The <span class="bg-gradient-to-br from-sky-700 to-slate-600/40 text-transparent bg-clip-text">Web</span>
 </h1>
