608e987cfcf03ee9517c5d02207c3c439d9aa8a5
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-07-11 18:54:55 -0700

    update(navbar): Navbar is now centered and fixed

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -1,26 +1,42 @@
-<nav id="navbar" class="fixed z-50 top-0 flex flex-row justify-end w-full h-16 bg-slate-50">
-  <ul class="list-none flex flex-row space-x-1 sm:space-x-6 pr-8 items-center lg:text-lg font-semibold">
-    <li><a class="navlink" href="#hero">Home</a></li>
-    <li><a class="navlink" href="#about">About</a></li>
-    <li><a class="navlink" href="#skills">Skills</a></li>
-    <li><a class="navlink" href="#contact">Contact</a></li>
+<script lang="ts">
+	import { onMount } from "svelte";
+
+  let scrollPosition = $state(0)
+  let scrollDirection = $state(0)
+  let isHidden = $derived(!scrollDirection)
+  
+  const handleScroll = (e: Event) => {
+    scrollDirection = window.scrollY > scrollPosition ? 1 : -1;
+    scrollPosition = window.scrollY
+  }
+
+  onMount(() => {
+    document.addEventListener("scroll", handleScroll);
+  })
+</script>
+
+<nav id="navbar" class={`
+  ${isHidden ? 'h-0' : 'h-16'} w-full 
+  fixed top-0 z-[100] flex
+  transition-all ease-in-out 
+  bg-white`
+}>
+  <ul class="flex w-full items-center justify-center lg:text-lg font-semibold list-none">
+    <a class="navlink" href="#hero"><li>Home</li></a>
+    <a class="navlink" href="#about"><li>About</li></a>
+    <a class="navlink" href="#skills"><li>Skills</li></a>
+    <a class="navlink" href="#contact"><li>Contact</li></a>
   </ul>
 </nav>
 
 <style>
-  li {
-    margin: 1rem;
-  }
-
-  .navlink {
-    display: block;
-    padding: 1rem;
-  }
-  
   li:hover {
     background-color: rgb(15 23 42 / var(--tw-bg-opacity, 1));
     box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.5);
     border-radius: 5px;
     color: white;
+  }
+  li {
+    padding: 1rem;
   }
 </style>
