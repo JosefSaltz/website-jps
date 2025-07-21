f52d18b4b0c7452d3f03a1732acac57717142f3b
Josef Saltz <JosefSaltz@users.noreply.github.com>
2025-07-20 18:42:29 -0700

    fix(SkillSection): Fix SkillSection's text scaling

--- src/lib/components/Skills/SkillSection.svelte
+++ src/lib/components/Skills/SkillSection.svelte
@@ -3,7 +3,7 @@
   let { name, skills }: Props = $props()
 
 </script>
-<section class="flex flex-col flex-wrap text-3xl">
+<section class="flex flex-col flex-wrap xl:text-3xl">
   <h4 class="font-bold">{name}</h4>
   <ul>
     {#each skills as skill}
